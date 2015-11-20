a722fe738e9c90d7778defd866aac50d8b504bb5
Wyatt Bushnell <wyatt@virsix.com>
2015-11-19 16:01:59 -0800

    got rid of all dynamic text

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,19 +6,11 @@ void ofApp::setup(){
   osc.setup(9999);
 
   movieSrc = "movies/test.mp4";
-  textBgSrc = "images/textBg.png";
   bgSrc = "images/passiveBg.png";
-  textBackground.load(textBgSrc);
   passiveBackground.load(bgSrc);
   bgRect = ofRectangle(0,0,ofGetWindowWidth(), ofGetWindowHeight()/5);
   bgRect.y = ofGetWindowHeight()-bgRect.height;
   player.loadMovie(movieSrc);
-
-  currText = "TESTIJN  NDFNKLDSN SNKLFNDSNG";
-
-  txtBox.init("monospace", 80);
-
-  txtBox.setText("HERE");
 }
 
 //--------------------------------------------------------------
@@ -31,9 +23,6 @@ void ofApp::update(){
       player.play();
     }else if(address == "/stopMovie"){
       player.stop();
-    }else if(address == "/setText"){
-      string txtToSet = m.getArgAsString(0);
-      setText(txtToSet);
     }
   }
   if(player.isPlaying()){
@@ -45,17 +34,12 @@ void ofApp::update(){
 void ofApp::draw(){
   if(player.isPlaying()){
     player.draw(0,0,ofGetWindowWidth(),ofGetWindowHeight());
-    textBackground.draw(bgRect.x,bgRect.y,bgRect.width,bgRect.height);
-    txtBox.draw(bgRect.x, bgRect.y);
   }else{
     passiveBackground.draw(0,0,ofGetWindowWidth(),ofGetWindowHeight());
   }
 }
 
 void ofApp::setText(string toSet){
-  currText = toSet;
-  txtBox.setText(currText);
-  //txtBox.wrapTextArea(ofGetWindowWidth(),ofGetWindowHeight());//bgRect.width, bgRect.height);
 }
 
 //--------------------------------------------------------------
